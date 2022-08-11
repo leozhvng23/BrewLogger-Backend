8422f1ebc65d2f6521d500b182026b1a188ea547
leozhvng23 <57277493+leozhvng23@users.noreply.github.com>
2022-08-11 16:51:05 -0400

    now fetches comment like status

--- controllers/comments-controllers.js
+++ controllers/comments-controllers.js
@@ -1,28 +1,40 @@
 const { validationResult } = require("express-validator");
 const db = require("../db");
 
+const uid = 6
+
 const getCommentsByRecipeId = async (req, res, next) => {
 	const queryText = `
-		SELECT users.name as user_name,
-			users.uid as uid,
-			comment_posted.time as posted_on,
-			comment_posted.content as content,
-			likes.count AS num_of_likes
-		FROM recipes
-			JOIN comment_posted USING(id)
-			JOIN users USING(uid)
-			LEFT JOIN (
-				SELECT cid, 
-					COUNT(*) AS count
-				FROM like_comment
-				GROUP BY cid
-			) AS likes USING(cid)
-		WHERE recipes.id = $1
-		ORDER BY comment_posted.time desc
+        SELECT 
+            comment_posted.cid as cid,
+            users.name as user_name,
+            users.uid as uid,    
+            comment_posted.time as posted_on,
+            comment_posted.content as content,
+            likes.count :: int AS num_of_likes,
+            isLiked.count as is_liked
+        FROM recipes
+            JOIN comment_posted USING(id)
+            JOIN users USING(uid)
+            LEFT JOIN (
+                SELECT cid, 
+                    COUNT(*) AS count
+                FROM like_comment
+                GROUP BY cid
+            ) AS likes USING(cid)
+            LEFT JOIN (
+                SELECT cid,
+                    COUNT(*) AS count
+                FROM like_comment 
+                WHERE uid = $2
+                GROUP BY cid
+            ) as isLiked USING(cid)
+        WHERE recipes.id = $1
+        ORDER BY comment_posted.time desc
 	`;
 	let result;
 	try {
-		result = await db.query(queryText, [req.params.id]);
+		result = await db.query(queryText, [req.params.id, uid]);
 	} catch (err) {
 		return next(new Error("Fetching comments failed, please try again later."));
 	}
