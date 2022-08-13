e2632a4c7d8c9c8e054dfbf6566fd138137ef3db
leozhvng23 <57277493+leozhvng23@users.noreply.github.com>
2022-08-13 17:03:11 -0400

    logs database string

--- app.js
+++ app.js
@@ -4,5 +4,8 @@ const mountRoutes = require("./routes");
 const app = express();
 mountRoutes(app);
 
+console.log("Database_URL", process.env.DATABASE_URL);
+
+
 app.listen(process.env.PORT || 5000, 
 	() => console.log("Server is running..."));
